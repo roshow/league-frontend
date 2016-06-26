0686a93fd09ea942f2e0b517b42aa10ced473509
roshow <rolando@roshow.net>
2016-06-26 00:07:08 -0400

    fixed nav active states

--- js/components/Nav/Nav.js
+++ js/components/Nav/Nav.js
@@ -7,19 +7,25 @@ export default class Nav extends React.Component {
 	}
 
 	render () {
-		const { rankings, matches, division, week, players, season } = this.props;
-    let seasonEls = [1, 2].map( seasonNo => {
+		let { rankings, matches, division, week, players, season } = this.props;
+		season = parseInt(season, 10);
+		week = parseInt(week, 10);
+    const seasonEls = [1, 2].map( seasonNo => {
       let classes = ( seasonNo === season) ? "active" : "";
       let targetUrl = week ? `/schedule/${division}/season/${seasonNo}/week/${week}` : `/rankings/${division}/season/${seasonNo}`;
       return <li className={classes} key={'sN' + seasonNo}><Link to={targetUrl}>SEASON {seasonNo}</Link></li>
     });
-    let divEls = ['argent', 'ultima'].map( div => {
+    const divEls = ['argent', 'ultima'].map( div => {
       let classes = ( div === division) ? "active" : "";
       return <li className={classes} key={div}><Link to={`/rankings/${div}/season/${season}`}>{div.toUpperCase()}</Link></li>
     });
     let weekNavStyles = {
       display: week ? 'block' : 'none'
     };
+    const weekEls = [1,2,3,4,5,6,7].map( weekNo => {
+      let className = weekNo ===  week ? "active" : "";
+      return <li className={className} key={`week${weekNo}`}><Link to={`/schedule/${division}/season/${season}/week/${weekNo}`} >Week {weekNo}</Link></li>
+    });
 
 		return (
 			<nav>
@@ -37,12 +43,7 @@ export default class Nav extends React.Component {
 	          <li className={ week ? 'active' : '' } ><Link to={`/schedule/${division}/season/${season}/week/1`}>Schedule</Link></li>
 	        </ul>
 	        <ul className="nav nav-pills pull-left" style={weekNavStyles}>
-	        { 
-	          [1,2,3,4,5,6,7].map( weekNo => {
-	            let className = weekNo ===  week ? "active" : "";
-	            return <li className={className} key={`week${weekNo}`}><Link to={`/schedule/${division}/season/${season}/week/${weekNo}`} >Week {weekNo}</Link></li>
-	          })
-	        }
+	        	{weekEls}
 	        </ul>
 	      </div>
       </nav>
