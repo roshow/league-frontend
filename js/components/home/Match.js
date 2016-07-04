5dd4b59f955c599d3bcc70c1fddd7692061f09ab
roshow <rolando@roshow.net>
2016-07-04 19:08:44 -0400

    added list links to matches.players

--- js/components/home/Match.js
+++ js/components/home/Match.js
@@ -13,13 +13,23 @@ export default class MatchSection extends React.Component {
     const matchScores = MatchStore.getMatches().map( match => (
       <ul className="list-group" key={match.match_id}>
         {
-          match.players.map( ({ name, destroyed }) => {
+          match.players.map( ({ name, destroyed, list_link }) => {
             let player = players[name];
             let _style = {};
-            if (match.winner === name) {
-              _style.fontWeight = 'bold';
+            let list = '';
+            if (match.played) {
+              if (match.winner === name) {
+                _style.fontWeight = 'bold';
+              }
+              list = list_link === '' ? ( 'List Missing' ) : ( <a href={list_link}>List</a> );
+              list = <span style={{fontWeight:'normal'}}>({list})</span>
             }
-            return ( <li className="list-group-item" key={name} style={_style}><span>{player.print_name}</span><span className="pull-right">{destroyed}</span></li> );
+            return (
+              <li className="list-group-item" key={name} style={_style}>
+                <span>{player.print_name} {list}</span>
+                <span className="pull-right">{destroyed}</span>
+              </li>
+            );
           })
         }
       </ul>
