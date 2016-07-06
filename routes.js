6b190813e45af999f8138de57810952aa1c62c63
roshow <rolando@roshow.net>
2016-07-06 19:36:08 -0400

    index redirect to argent season 2 rankings; changed og image to season 2 banner

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router, Route, IndexRoute} from 'react-router';
+import {Router, Route, IndexRoute, IndexRedirect} from 'react-router';
 import App from './js/components/app';
 import HomeIndex from './js/components/home/index';
 import Matches from './js/components/home/Match';
@@ -9,7 +9,7 @@ import Players from './js/components/Players/Players';
 
 export default (
 	 <Route path="/" component={HomeIndex}>
-	 	<IndexRoute component={Rankings} />
+	 	<IndexRedirect to="/rankings/argent/season/2" />
 	 	<Route path="players" component={Players}/>
     <Route path="rankings/:division/season/:season" component={Rankings}/>
 		<Route path="schedule/:division/season/:season/week/:week" component={MatchesIndex}/>
